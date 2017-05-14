1a6c6ff34f665e61dd3e3394cab3cc45511ddc6c
Camila <cruz.camila001@gmail.com>
2017-05-14 17:53:31 -0300

    Adição de animação no menu lateral

--- src/js/menu.js
+++ src/js/menu.js
@@ -45,3 +45,13 @@ document.getElementById("menuLateral").innerHTML =
     '</div>' +
 '</div>'
 ;
+
+// Adiciona uma animação para baixo quando o drpdown se expande
+$('.dropdown').on('show.bs.dropdown', function() {
+    $(this).find('.dropdown-menu').first().stop(true, true).slideDown();
+});
+
+// Adiciona uma animação para cima quando o dropdown se retrai
+$('.dropdown').on('hide.bs.dropdown', function() {
+    $(this).find('.dropdown-menu').first().stop(true, true).slideUp();
+});
