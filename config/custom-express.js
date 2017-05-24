ccc2eae59a8d7bd8a6cf9f411e2dd60a1d539cb9
ycabral <y.cabral18@hotmail.com>
2017-05-24 13:44:10 -0300

    Inicializa projeto NodeJS

---
+++ config/custom-express.js
@@ -0,0 +1,24 @@
+var express = require('express');
+var consign = require('consign');
+var morgan = require('morgan');
+var logger = require('./../servicos/logger.js');
+
+module.exports = function(){
+  var app = express();
+
+  app.use(morgan('common', {
+    stream: {
+      write: function( mensagem ) {
+        logger.info(mensagem);
+      }
+    }
+  }));
+
+  consign()
+   .include('controllers')
+   .then('services')
+   .then('views')
+   .into(app);
+
+  return app;
+};
---
+++ index.js
@@ -0,0 +1,5 @@
+var app = require('./config/custom-express')();
+
+app.listen(3000 , () => {
+    console.log('Servidor up and running, escutando porta 3000.');
+})
