a470b6c52995865ad2b052451961684d0edbcb41
Yuri Cabral <y.cabral18@hotmail.com>
2017-06-02 15:10:35 -0300

    Revert "Instala jquery e bootstrap via npm"
    This reverts commit 7faf70bd32dfcac8482f04aa013c8db802bff530.

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 var app = require('./config/custom-express')();
 
 app.listen(3000 , () => {
-    console.log('Servidor up and running, escutando porta 3000 ;)');
+    console.log('Servidor up and running, escutando porta 3000.');
 })
