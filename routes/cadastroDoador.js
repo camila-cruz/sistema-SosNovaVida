3cb7fc37d4776670d4496f537f55a5864695d74b
Yuri Cabral <y.cabral18@hotmail.com>
2017-06-19 22:05:40 -0300

    Merge NodeFront

--- routes/cadastroDoador.js
+++
@@ -1,6 +0,0 @@
-module.exports = ( app ) => {
-    app.get('/cadastro/doador', ( req, res ) => {
-        console.log('Requisicão recebida em /cadastro/doador');
-        res.render('views/login');
-    })
-};
