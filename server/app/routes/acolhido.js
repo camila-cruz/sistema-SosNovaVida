2a0bc6522995c2ca0440a45ede5b512d334cc312
ycabral <y.cabral18@hotmail.com>
2017-09-24 19:21:51 -0300

    Faz consulta ao pia funcionar e tras todos os acolhidos na consulta

--- server/app/routes/acolhido.js
+++ server/app/routes/acolhido.js
@@ -12,15 +12,19 @@ module.exports = ( app ) => {
                 console.log(err.message);
                 return res.status(500).json({success: false, data: err});
             } else {
-                let linhas = result.rows[0];
-                acolhidos.push(linhas);
+
+                result.rows.forEach( (elemento) => {
+                    acolhidos.push(elemento);
+                });
+                //let linhas = result.rows[0];
+                //acolhidos.push(linhas);
 
                 return res.json(acolhidos);
                 res.send(acolhidos).status(200);
             };
         });
     });
-
+/* tirar código chumbado. Apagar
     app.get('/acolhidos/pia/:id', (req, res) => {
         let id = req.params.id;
         console.log("ID: " + id);
@@ -39,32 +43,18 @@ module.exports = ( app ) => {
         res.send(acolhido).status(200);
         //res.render('formPIA');
     });
-
-    /*
+*/ 
+    
     app.get('/acolhidos/pia/:id', (req, res) => {
         let id = req.params.id;
         console.log('Recebendo requisição GET em /acolhidos/pia/' + id);
         // Get a Postgres client from the connection pool
-        pg.connect(connectionString, (err, client, done) => {
-            // Handle connection errors
-            if(err) {
-                done();
-                console.log(err);
-                return res.status(500).json({success: false, data: err});
-            }
-            // SQL Query > Select Data
-            console.log('ID buscado: ' + id);
-            const query = client.query('SELECT * FROM acolhidos WHERE idacolhido = $1;', [id] );
-            // Stream acolhidos back one row at a time
-            query.on('row', (row) => {
-                done();
-                console.log(row);
-                return res.send(row).status(200);
-            });
+        con.query('SELECT * FROM acolhidos WHERE idacolhido = $1;', [id], ( err, results ) => {
+            if (err) return res.status(500).send(err);
+
+            return res.status(200).send(results.rows[0]);
         });
     });
-    */
-
 
     app.post('/acolhidos', (req, res, next) => {
         const data = req.body;
