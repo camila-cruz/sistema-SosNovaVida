3908a13fba71009732aa059c8d1708d48c3e0b33
Yuri Cabral <y.cabral18@hotmail.com>
2017-10-26 07:47:19 -0200

    retira código chumbado

--- server/app/routes/acolhido.js
+++ server/app/routes/acolhido.js
@@ -25,26 +25,6 @@ module.exports = ( app ) => {
             };
         });
     });
-/* tirar código chumbado. Apagar
-    app.get('/acolhidos/pia/:id', (req, res) => {
-        let id = req.params.id;
-        console.log("ID: " + id);
-        //console.log(req.params);
-        const acolhido = {
-            id: 14,
-            nome: 'José da Silva',
-            data_nasc: new Date(),
-            local_nasc: 'Suzano - SP',
-            nome_mae: 'Maria da Silva',
-            nome_pai: 'João da Silva'
-            //imagem: "http://animais.culturamix.com/blog/wp-content/gallery/fotos-de-capivara-1/Fotos-Capivara-1.jpeg"
-        };
-        //res.json(acolhidos[id-1]);
-        //res.json(acolhido);
-        res.send(acolhido).status(200);
-        //res.render('formPIA');
-    });
-*/ 
     
     app.get('/acolhidos/:id', (req, res) => {
         let id = req.params.id;
@@ -53,6 +33,7 @@ module.exports = ( app ) => {
         con.query('SELECT * FROM acolhidos WHERE idacolhido = $1;', [id], ( err, results ) => {
             if (err) return res.status(500).send(err);
 
+            console.log( results.rows[0]);
             return res.status(200).send(results.rows[0]);
         });
     });
@@ -72,39 +53,6 @@ module.exports = ( app ) => {
                 return res.status(200); //Status 200 - Created. O Front-end é responsável por fazer o resto.
             };
         });
-/*
-        const results = [];
-        // Grab data from http request
-        const data = {
-            text: req.body.text,
-            complete: false
-        };
-
-        // Get a Postgres client from the connection pool
-        pg.connect(connectionString, (err, client, done) => {
-            // Handle connection errors
-            if(err) {
-                done();
-                console.log(err);
-                return res.status(500).json({success: false, data: err});
-            }
-            // SQL Query > Insert Data
-            client.query('INSERT INTO acolhidos(nome, data_nasc, data_entrada, local_nasc, uf, nome_mae, maeresp, nome_pai, pairesp, nomeresp) values($1, $2, $3, $4, $5, $6, $7, $8, $9, $10)',
-            [data.nome, data.dtNasc, data.dtEntr, data.cidNatal, data.uf.nome, data.nomeMae, data.isMaeResp, data.nomePai, data.isPaiResp, data.outroResp]);
-            console.log("DATA");
-            console.log(data);
-            // SQL Query > Select Data
-            const query = client.query('SELECT * FROM acolhidos ORDER BY idacolhido ASC');
-            // Stream results back one row at a time
-            query.on('row', (row) => {
-                results.push(row);
-            });
-            // After all data is returned, close connection and return results
-            query.on('end', () => {
-                done();
-                return res.json(results);
-            });
-        });*/
     });
 
 }
