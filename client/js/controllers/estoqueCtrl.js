83000dd0d0f8fd04e5f487fbdcb0d59f6bb56b27
Yuri Cabral <y.cabral18@hotmail.com>
2017-11-08 14:20:07 -0200

    Criação de lista iniciada

--- client/js/controllers/estoqueCtrl.js
+++ client/js/controllers/estoqueCtrl.js
@@ -1,6 +1,10 @@
 angular.module('novaVida').controller('estoqueCtrl', function( $scope, estoque, estoqueAPI ) {
     $scope.produtos = estoque.data;
     $scope.modoDeAbertura = "criar";
+    $scope.selecionados = [];
+    $scope.lista = {
+        itens: []
+    }
 
     $scope.movimentaEstoque = ( produto, metodo ) => {
         produto.metodo = metodo
@@ -26,5 +30,25 @@ angular.module('novaVida').controller('estoqueCtrl', function( $scope, estoque, 
 
     const atualizaProdutos = () => {
         estoqueAPI.getEstoque().then( results => $scope.produtos = results.data );
+    }
+
+    $scope.criarLista = ( produtos ) => {
+        $scope.modoDeAbertura = "criar";
+
+        produtos.filter( ( produto ) => {
+            if ( produto.selecionado ) {
+                $scope.selecionados.push( produto );
+                $scope.lista.itens.push( produto );
+            }; 
+        });
+
+        console.log( $scope.selecionados );
+    }
+
+    $scope.salvarLista = () => console.log('Salvar lista.');
+
+    $scope.adicionarItemNaLista = ( novoItem ) => {
+        $scope.lista.itens.push( novoItem );
+        $scope.novoItem = {};
     }
 });
