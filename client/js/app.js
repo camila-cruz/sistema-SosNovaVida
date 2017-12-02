33a493a676da67e93d321031683a0f097ed7e20f
Camila <cruz.camila001@gmail.com>
2017-12-02 20:45:53 -0200

    Remove alert de login no sistema

--- client/js/app.js
+++ client/js/app.js
@@ -9,13 +9,12 @@ angular.module('novaVida').controller('inicioController', function( $scope, logi
         loginAPI
             .postLogin(usuario)
             .then(results => {
-                swal("Bem-Vindo(a)", "Bem-vindo(a) ao sistema da SOS Nova Vida.", "success"); 
+                //swal("Bem-Vindo(a)", "Bem-vindo(a) ao sistema da SOS Nova Vida.", "success"); 
                 $scope.isLogado = true;
                 $location.path('/index');
             })
             .catch(err => { 
                 $scope.usuario = {};
-                //const statusCode = err.status;
 
                 return swal("Opa...", "Usuário ou senha incorretos!", "error");
             });
