acf6184e70d9017ba96e76ecf76308730bedfb71
Camila <cruz.camila001@gmail.com>
2017-12-02 17:12:25 -0200

    Adiciona mensagens de usuário no cadastro e consulta de doador

--- client/js/controllers/doadorCtrl.js
+++ client/js/controllers/doadorCtrl.js
@@ -4,42 +4,56 @@ angular.module('novaVida').controller('doadorCtrl', function( $scope, $location,
 
     $scope.cadastrarDoador = ( doador ) => {
         doadorAPI.postDoador( doador )
-          .then( response => {
-            alert('Doador cadastrado com sucesso!');
-            $location.path('/consulta/doador')
-          })
-          .catch( err => {
-            console.log( err );
-          });
+            .then( response => {
+                swal("Sucesso!", "Doador cadastrado com sucesso!", "success");
+                $location.path('/consulta/doador')
+            })
+            .catch( err => {
+                swal("Opa...", "Houve um erro, tente novamente!", "error");
+                console.log( err );
+            });
     };
 
     $scope.excluirSelecionados = ( doadores ) => {
-      if ( confirm('Deseja realmente excluir os doadores selecionados?') ) {
-        const doadoresSelecionados = doadores.filter( doador => {
-          if ( doador.selecionado ) {
-            return doador.id;
-          }
+
+        swal({
+            title: 'Atenção',
+            text: "Deseja realmente excluir os doadores selecionados?",
+            type: 'warning',
+            showCancelButton: true,
+            confirmButtonColor: '#3085d6',
+            cancelButtonColor: '#d33',
+            cancelButtonText: 'Cancelar',
+            confirmButtonText: 'Sim'
+        }).then((result) => {
+            if (result.value) {
+                const doadoresSelecionados = doadores.filter( doador => {
+                    if ( doador.selecionado ) {
+                        return doador.id;
+                    }
+                });
+            
+                const doadoresRemanescentes = doadores.filter( doador => {
+                    if ( !doador.selecionado ) {
+                        return doador.id;
+                    }
+                });
+                console.log( doadoresSelecionados );
+        
+                doadorAPI.deleteDoador( doadoresSelecionados )
+                    .then( response => {
+                        swal("Sucesso!", "Doador apagado com sucesso!", "success");
+                        $scope.doadores = doadoresRemanescentes;
+                    })
+                    .catch( err => {
+                        swal("Opa...", "Houve um erro, tente novamente!", "error");
+                        console.log( response )
+                    });
+            }       
         });
-
-        const doadoresRemanescentes = doadores.filter( doador => {
-          if ( !doador.selecionado ) {
-            return doador.id;
-          }
-         });
-        console.log( doadoresSelecionados );
-
-        doadorAPI.deleteDoador( doadoresSelecionados )
-          .then( response => {
-            $scope.doadores = doadoresRemanescentes;
-          })
-          .catch( err => {
-            console.log( response )
-          });
-      }
     };
 
     $scope.voltar = () => {
         $location.path('/consulta/doador')
     }
-
 });
